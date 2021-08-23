a0a892bad3f150c7218aa9f5f34b7fa687ec2975
milinfic <paulo.cpa@hotmail.com>
2021-08-23 17:12:40 -0300

    first and last commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ExercicioOrderItem-CursoUdemyJavaCompleto-
---
+++ src/aplication/program.java
@@ -0,0 +1,65 @@
+package aplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.Client;
+import model.entities.Order;
+import model.entities.OrderItem;
+import model.entities.Product;
+import model.enums.OrderStatus;
+
+public class program {
+
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter cliente date");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Birth Date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter Order Date:");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter #" + i + " item data:");
+            System.out.print("Product name: ");
+            sc.nextLine(); // para pegar os espaços
+            String productName = sc.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = sc.nextDouble();
+
+            Product product = new Product(productName, productPrice);
+
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+
+            OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+
+            order.addItem(orderItem);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMMARY:");
+        System.out.println(order);
+
+        sc.close();
+
+    }
+
+}
